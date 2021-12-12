6206ed9df286760dfbe8b2ee7fd1a313637c87c1
Obtez <59846143+Obtez@users.noreply.github.com>
2021-12-12 18:46:05 +0100

    register form

--- client/src/components/forms/auth-forms/RegisterForm.tsx
+++ client/src/components/forms/auth-forms/RegisterForm.tsx
@@ -1,15 +1,17 @@
 import { ChangeEvent, FormEvent, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import { UserLogin } from "../../../types/types";
+import { UserRegister } from "../../../types/types";
 
-const emptyUser: UserLogin = {
+const emptyUser: UserRegister = {
+  userName: "",
   email: "",
-  password: ""
+  password: "",
+  confirmPassword: ""
 }
 
-function LoginForm() {
-  const [userData, setUserData] = useState<UserLogin>(emptyUser)
+function RegisterForm() {
+  const [userData, setUserData] = useState<UserRegister>(emptyUser)
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -22,13 +24,28 @@ function LoginForm() {
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    console.log(userData);
+    const response = await axios.post("http://localhost:8090/api/users", userData);
+    console.log(response);
   }
 
   return (
     <div className="container">
       <h1 className="text-center text-3xl text-gray-700 mb-6">Login</h1>
       <form className="w-full max-w-sm m-auto" onSubmit={handleSubmit}>
+
+        <div className="md:flex md:items-center mb-6">
+            <label className="block text-gray-500 font-bold mb-1" htmlFor="userName">Username</label>
+          <input
+            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+            type="text"
+            name="userName"
+            id="userName"
+            value={userData.userName}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        
         <div className="md:flex md:items-center mb-6">
             <label className="block text-gray-500 font-bold mb-1" htmlFor="email">Email</label>
           <input
@@ -54,14 +71,26 @@ function LoginForm() {
             required />
         </div>
 
+      <div className="md:flex md:items-center mb-6">
+        <label className="block text-gray-500 font-bold mb-1" htmlFor="confirmPassword">Confirm Password</label>
+        <input
+          className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+          type="password"
+          name="confirmPassword"
+          id="confirmPassword"
+          value={userData.confirmPassword}
+          onChange={handleChange}
+          required />
+      </div>
+
       <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded" type="submit">Login</button>
       </form>
 
       <div className="block text-center mt-6 text-purple-700 hover:text-purple-500">
-        <Link to="/register">No account yet? Register here.</Link>
+        <Link to="/login">Already have an account? Login here.</Link>
       </div>
     </div>
   )
 }
 
-export default LoginForm
+export default RegisterForm
--- client/src/router/Router.tsx
+++ client/src/router/Router.tsx
@@ -3,6 +3,7 @@ import Layout from "../components/layout/Layout";
 import Login from "../pages/login/Login";
 import NewSubscription from "../pages/new-subscription/NewSubscription";
 import Overview from "../pages/overview/Overview";
+import Register from "../pages/register/Register";
 import Subscriptions from "../pages/subscriptions/Subscriptions";
 
 function Router() {
@@ -10,7 +11,11 @@ function Router() {
     <BrowserRouter>
       <Layout>
         <Routes>
+          {/* PUBLIC ROUTES */}
           <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          {/* PRIVATE ROUTES */}
           <Route path="/overview" element={<Overview />} />
           <Route path="/subscriptions" element={<Subscriptions />} />
           <Route path="/new-subscription" element={<NewSubscription />} />
