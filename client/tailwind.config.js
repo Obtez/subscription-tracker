59bf0f2c8cb6d2e727a2bf1a9b731ccfecd29d0f
Obtez <59846143+Obtez@users.noreply.github.com>
2021-12-12 09:57:04 +0100

    implemented tailwind css

---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+// const colors = require("tailwindcss/colors")
+
+module.exports = {
+ purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+ ],
+}
